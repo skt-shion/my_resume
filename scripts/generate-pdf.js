524adf95466b2aeb3cd6e6bfbe18ebdcfed6a181
shikano.takeki <distortion0mix@gmail.com>
2026-06-17 20:39:53 +0900

    fix: set executable path

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -43,7 +43,6 @@ async function main() {
 </html>`;
 
   const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
